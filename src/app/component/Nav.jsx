ca7c3719cef9e3e32ee4b8a5b1516ef9edaa1f4b
Sekiuu <sega.pamma@gamil.com>
2025-04-22 21:21:44 +0700

    seceond comit

---
+++ src/app/component/Nav.jsx
@@ -0,0 +1,35 @@
+'use client'
+import React from 'react'
+import Link from 'next/link'
+import { useState } from 'react'
+
+function Nav() {
+    const [open, setOpen] = useState(false)
+    return (
+        <nav className='max-md:fixed flex absolute w-full mb-6 max-md:py-2 py-5 px-[5%] z-1 justify-between items-center' >
+            <div className='md:text-2xl z-1 max-md:text-[.75rem]'>บริษัท นิยมคอนสตรัคชั่น<br />รับเหมาก่อสร้าง</div>
+            <div className={`text-1xl flex justify-between items-center
+            ${open ? 'visible z-51 absolute inset-x-1 inset-y-20 flex-col' :
+                    'max-md:hidden z-1'}`}>
+                <Link href={'#'} className='max-md:border-y max-md:w-full max-md:py-[5%] hover:cursor-pointer hover:font-bold mx-3 px-2 text-center'>หน้าแรก</Link>
+                <Link href={'#'} className='max-md:border-b max-md:w-full max-md:py-[5%] hover:cursor-pointer hover:font-bold mx-3 px-2 text-center'>บริการ</Link>
+                <Link href={'#'} className='max-md:border-b max-md:w-full max-md:py-[5%] hover:cursor-pointer hover:font-bold mx-3 px-2 text-center'>ติดต่อ</Link>
+                <Link href={'#'} className='max-md:border-b max-md:w-full max-md:py-[5%] hover:cursor-pointer hover:font-bold text-center md:border-1 px-5 py-2'>โทรเลย</Link>
+            </div>
+            {/* panel */}
+            <div className={`fixed inset-0 z-40 backdrop-blur-sm transition-all duration-300 ${open ? 'visible' : 'hidden'
+                }`} onClick={() => setOpen(false)} />
+
+            <button onClick={() => setOpen(!open)} className={`absolute p-2 z-50 right-5 ${open ? 'visible' : 'md:hidden'
+                }`}>
+                Menu
+            </button>
+
+            {/* black drop */}
+            <div className={`inset-0  bg-black opacity-10
+                ${open ? '' : 'absolute'}`} />
+        </nav>
+    )
+}
+
+export default Nav
---
+++ src/app/component/Service_intro.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+function Service_intro() {
+    return (
+        <section id='services-intro' className='md:p-16 md:pt-30 w-full bg-neutral-800'>
+            <div className='grid lg:grid-cols-4 grid-cols-2 border border-amber-600'>
+                <div className='aspect-square col-span-2 border border-amber-600'>
+                    <Image src={'/pic/team1.jpg'} width={1024} height={1024} sizes='50vw' alt='team1'
+                        className='object-cover aspect-square' />
+                </div>
+                <div className='col-span-2 border border-amber-600 aspect-square text-center overflow-hidden'>
+                    <div className={'flex flex-col w-full h-full justify-center kanit'}>
+                        <h1 className='text-6xl font-bold mb-10 lg:mb-6 xl:text-6xl lg:text-5xl
+                         max-md:text-4xl max-md:mb-5'>บริการของเรา</h1>
+                        <div className='text-neutral-300 text-left lg:text-1xl text-2xl font-thin space-y-8 
+                        lg:space-y-5 px-[20%] max-md:space-y-3 max-md:text-[1rem]'>
+                            <li className='hover:text-neutral-100'><Link href={'#'}>งานปรับปรุงบ้าน</Link></li>
+                            <li className='hover:text-neutral-100'><Link href={'#'}>งานปรับปรุงห้องครัว</Link></li>
+                            <li className='hover:text-neutral-100'><Link href={'#'}>งานปรับปรุงห้องน้ำ</Link></li>
+                            <li className='hover:text-neutral-100'><Link href={'#'}>งานตกแต่งภายใน </Link></li>
+                            <li className='hover:text-neutral-100'><Link href={'#'}>บริการปูพื้น</Link></li>
+                        </div>
+                        <Link href={'#'} className='text-center w-[50%] mx-auto mt-10 lg:py-3 py-5 bg-amber-800
+                        max-md:mt-5 max-md:py-2'>ดูข้อมูล</Link>
+                    </div>
+                </div>
+
+                <div className='col-span-1 row-span-1 border border-amber-600 aspect-square flex justify-center flex-col items-center'>
+                    <Image width={200} height={200} src={'/icon/user-profile-group_white.svg'} alt='group' className='w-1/2' />
+                    <h1 className='text-neutral-400 mt-2 lg:mt-5 text-3xl lg:text-2xl 
+                    max-md:text-xl'>ตัวอย่าง</h1>
+                </div>
+                <div className='col-span-1 row-span-1 border border-amber-600 aspect-square flex justify-center flex-col items-center'>
+                    <Image width={200} height={200} src={'/icon/user-profile-group_white.svg'} alt='group' className='w-1/2' />
+                    <h1 className='text-neutral-400 mt-2 lg:mt-5 text-3xl lg:text-2xl 
+                    max-md:text-xl'>ตัวอย่าง</h1>
+                </div>
+                <div className='col-span-1 row-span-1 border border-amber-600 aspect-square flex justify-center flex-col items-center'>
+                    <Image width={200} height={200} src={'/icon/user-profile-group_white.svg'} alt='group' className='w-1/2' />
+                    <h1 className='text-neutral-400 mt-2 lg:mt-5 text-3xl lg:text-2xl 
+                    max-md:text-xl'>ตัวอย่าง</h1>
+                </div>
+                <div className='col-span-1 row-span-1 border border-amber-600 aspect-square flex justify-center flex-col items-center'>
+                    <Image width={200} height={200} src={'/icon/user-profile-group_white.svg'} alt='group' className='w-1/2' />
+                    <h1 className='text-neutral-400 mt-2 lg:mt-5 text-3xl lg:text-2xl 
+                    max-md:text-xl'>ตัวอย่าง</h1>
+                </div>
+
+                <div className='col-span-2 border border-amber-600 md:aspect-square text-center overflow-hidden'>
+                    <div className='flex flex-col mx-auto w-[80%] py-[10%] xl:py-[15%] text-left'>
+                        <h1 className='text-6xl max-md:text-5xl font-bold'>เกี่ยวกับเรา</h1>
+                        <h2 className='mt-15 text-[2rem] max-md:text-[1.5rem] max-xl:mt-5 text-neutral-400'>บริษัท นิยมคอนสตรัคชั่น<br />รับเหมาก่อสร้าง</h2>
+                        <p className='lg:mt-10 mt-20 max-md:mt-10 text-2xl max-md:text-[1rem]'>
+                            นี่คือส่วนของเนื้อหา เพิ่มข้อความของคุณและแก้ไขได้ง่ายๆ
+                            ที่นี่ เพียงคลิก "แก้ไขข้อความ" หรือดับเบิลคลิกจากนั้นลงเนื้อหาของคุณ หรือแก้ไข
+                            หรือเปลี่ยนแบบฟอนต์ได้เลย ที่ตรงนี้เหมาะอย่างยิ่งสำหรับการบอกเล่าเรื่องราวและแนะนำตัวให้ผู้ใช้รู้จักคุณมากยิ่งขึ้น
+                            มาเริ่มสร้างสรรค์เนื้อหาของคุณเลย
+                        </p>
+                    </div>
+                </div>
+                <div className='aspect-square col-span-2 border border-amber-600 '>
+                    <Image src={'/pic/team2.jpg'} width={1024} height={1024} sizes='50vw' alt='team2'
+                        className='object-cover aspect-square' />
+                </div>
+            </div>
+
+        </section>
+    )
+}
+
+export default Service_intro
