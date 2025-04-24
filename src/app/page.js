792d62935a2ddd35794782749fddc90eb19f8e06
Sekiuu <sega.pamma@gamil.com>
2025-04-24 18:34:44 +0700

    remove ompany info from page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,6 @@
 import Intro from "./component/Intro";
 import Service_intro from "./component/Service_intro";
 
-export const metadata = {
-  title: companyInfo.company_name + " ปทุมธานี และ จังหวัดไกล้เคียง",
-    description: "รับเหมางานไฟฟ้า ปทุมธานี | บริษัท นิยมดีอิเล็กทริกซ์ บริการติดตั้งระบบไฟฟ้า บ้าน อาคาร โรงงาน เดินสายไฟ ซ่อมตู้ไฟ ปรับปรุงระบบไฟ ไฟดับ รับเดินสายไฟในบ้าน ไฟรั่ว เรียกช่าง โดยทีมช่างมืออาชีพ ประสบการณ์ยาวนาน บริการทั่วปทุมธานี"
-  
-};
-
 export default function Home() {
   return (
     <div>
